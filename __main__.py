d1ff55e3be49c5b462aa99d4df1776f2648c03c1
Dan <daniel@x-matik.com>
2020-10-16 16:12:51 -0400

    add some options

--- __main__.py
+++ __main__.py
@@ -1,20 +1,29 @@
 from pynput import keyboard
 
+import argparse
 import os
 import re
 import signal
 import subprocess
 import time
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', default='danscap')
+parser.add_argument('--extension', '--ext', '-e', choices=['mkv', 'gif'], default='mkv')
+parser.add_argument('--countdown', type=int, default=3)
+parser.add_argument('--framerate', type=int, default=18)
+parser.add_argument('--end-key', default='esc')
+args = parser.parse_args()
+
 # constants
-file_name = os.path.join(os.path.dirname(__file__), 'danscap.gif')
+file_name = os.path.join(os.path.dirname(__file__), f'{args.name}.{args.extension}')
 w, h = re.search(r'(\d+)x(\d+).*\*', subprocess.run('xrandr', capture_output=True).stdout.decode()).groups()
 
 # ensure file doesn't exist
 if os.path.exists(file_name): os.remove(file_name)
 
 # give user some time to set up the scene
-for i in range(3, 0, -1):
+for i in range(args.countdown, 0, -1):
     print(i)
     time.sleep(1)
 
@@ -22,14 +31,15 @@ for i in range(3, 0, -1):
 p = subprocess.Popen([
     'ffmpeg',
     '-video_size', f'{w}x{h}',
-    '-framerate', '18',
+    '-framerate', str(args.framerate),
     '-f', 'x11grab',
     '-i', ':0.0',
     file_name,
 ])
 
 # listen for esc
-with keyboard.Listener(on_press=lambda key: key != keyboard.Key.esc) as listener:
+end_key = getattr(keyboard.Key, args.end_key)
+with keyboard.Listener(on_press=lambda key: key != end_key) as listener:
     listener.join()
 
 # stop recording
