7cdedd8b69b8b3d70f2361f6956015bbe9a53eaf
Dan <daniel@foresightanalytics.ca>
2022-02-10 16:06:44 -0500

    partial fix, Ubuntu has builtin screencasting now (ctrl-alt-shift-r)

--- __main__.py
+++ __main__.py
@@ -1,11 +1,8 @@
-from pynput import keyboard
-
 import argparse
 import os
 import re
 import signal
 import subprocess
-import threading
 import time
 
 parser = argparse.ArgumentParser()
@@ -17,11 +14,16 @@ parser.add_argument('--end-key', default='esc')
 args = parser.parse_args()
 
 # constants
-file_name = os.path.join(os.path.dirname(__file__), f'{args.name}.{args.extension}')
-w, h = re.search(r'(\d+)x(\d+).*\*', subprocess.run('xrandr', capture_output=True).stdout.decode()).groups()
+DIR = os.path.dirname(os.path.realpath(__file__))
+
+FILE_PATH = os.path.join(DIR, f'{args.name}.{args.extension}')
+W, H = re.search(r'(\d+)x(\d+).*\*', subprocess.run('xrandr', capture_output=True).stdout.decode()).groups()
+
+# start esc listener
+listener = subprocess.Popen(['sudo', 'python3', os.path.join(DIR, 'listener.py')])
 
 # ensure file doesn't exist
-if os.path.exists(file_name): os.remove(file_name)
+if os.path.exists(FILE_PATH): os.remove(FILE_PATH)
 
 # give user some time to set up the scene
 for i in range(args.countdown, 0, -1):
@@ -29,35 +31,21 @@ for i in range(args.countdown, 0, -1):
     time.sleep(1)
 
 # start recording
-p = subprocess.Popen([
+ffmpeg = subprocess.Popen([
     'ffmpeg',
-    '-video_size', f'{w}x{h}',
+    '-video_size', f'{W}x{H}',
     '-framerate', str(args.framerate),
     '-f', 'x11grab',
     '-i', ':0.0',
-    file_name,
+    FILE_PATH,
 ])
 
-# listen for esc
-end_key = getattr(keyboard.Key, args.end_key)
-end_key_pressed = False
-
-def listen_for_end_key():
-    def on_press(key):
-        global end_key_pressed
-        if key == end_key:
-            end_key_pressed = True
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
-
-thread = threading.Thread(target=listen_for_end_key)
-thread.daemon = True
-thread.start()
-while not end_key_pressed: time.sleep(0.01)
+# wait for esc
+listener.wait()
 
 # stop recording
-p.send_signal(signal.SIGINT)
+ffmpeg.send_signal(signal.SIGINT)
 time.sleep(1)
 
 # pop up window for dragging
-subprocess.run(['xdg-open', os.path.dirname(file_name)])
+subprocess.run(['xdg-open', os.path.dirname(FILE_PATH)])
